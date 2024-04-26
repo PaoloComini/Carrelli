f332cf4851ac8d204fdf8efccc850f463be2453b
PaoloComini <paolocomini@olivelliputelli.it>
2024-04-26 12:25:03 +0200

    buonasera

--- database.js
+++ database.js
@@ -2,10 +2,10 @@
     const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
     console.log();
     const connpool = mysql.createPool({
-        host: "172.24.48.1",
+        host: "10.20.2.173",
         user: "tps",
         password: "ttppss",
-        database: "td"
+        database: "prenotazioni"
     }, { debug: true });
     
     connpool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
